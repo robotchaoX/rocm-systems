f2249af92caa055706ab753be69ebc07a062803d
Venkateshwar Reddy Kandula <venkateshwar.kandula1306@gmail.com>
2026-02-09 03:23:25 -0600

    Revert "Add TheRock packaging mode and improve installed unit-test usability"
    This reverts commit aabf86e1406b3b785895567ce1b6bd6371f2f274.

--- projects/rocprofiler-sdk/cmake/rocprofiler_config_install_tests.cmake
+++ projects/rocprofiler-sdk/cmake/rocprofiler_config_install_tests.cmake
@@ -11,56 +11,44 @@ set(${PACKAGE_NAME}_BUILD_TREE
     ON
     CACHE BOOL "" FORCE)
 
-set(_ROCPROFILER_INSTALL_TEST_COMPONENT OFF)
-if(ROCPROFILER_BUILD_TESTS OR ROCPROFILER_THEROCK_PACKAGING)
-    set(_ROCPROFILER_INSTALL_TEST_COMPONENT ON)
-endif()
-
-# do not install the package config if tests are not requested
-if(NOT _ROCPROFILER_INSTALL_TEST_COMPONENT)
+# do not install the package config if tests are not built
+if(NOT ROCPROFILER_BUILD_TESTS)
     return()
 endif()
+
+install(
+    EXPORT ${PACKAGE_NAME}-targets
+    FILE ${PACKAGE_NAME}-targets.cmake
+    DESTINATION ${CMAKE_INSTALL_LIBDIR}/cmake/${PACKAGE_NAME}
+    COMPONENT tests)
+
+rocprofiler_install_env_setup_files(
+    NAME ${PACKAGE_NAME}
+    VERSION ${PROJECT_VERSION}
+    INSTALL_DIR ${CMAKE_INSTALL_DATAROOTDIR}
+    COMPONENT tests)
 
 # ------------------------------------------------------------------------------#
 # install tree
 #
 set(PROJECT_INSTALL_DIR ${CMAKE_INSTALL_PREFIX})
-set(TEST_ROOT_DIR ${CMAKE_INSTALL_DATAROOTDIR}/${PROJECT_NAME}/tests)
-set(INCLUDE_INSTALL_DIR ${TEST_ROOT_DIR}/unit-tests)
+set(INCLUDE_INSTALL_DIR ${CMAKE_INSTALL_DATAROOTDIR}/${PROJECT_NAME}/tests/unit-tests)
 string(REPLACE "-" "_" PACKAGE_NAME_UNDERSCORED ${PACKAGE_NAME})
-set(PROJECT_INCLUDE_FILES "")
-set(PROJECT_HAS_TEST_TARGETS OFF)
-set(PACKAGE_TEST_LD_LIBRARY_PATH
-    "${CMAKE_INSTALL_PREFIX}/${CMAKE_INSTALL_LIBDIR}:${CMAKE_INSTALL_PREFIX}/llvm/lib")
+get_property(
+    _ROCP_SDK_TEST_TARGETS
+    DIRECTORY ${CMAKE_SOURCE_DIR}
+    PROPERTY rocprofiler-sdk-tests-targets)
 
-if(ROCPROFILER_BUILD_TESTS OR ROCPROFILER_THEROCK_PACKAGING)
-    set(PROJECT_HAS_TEST_TARGETS ON)
-    install(
-        EXPORT ${PACKAGE_NAME}-targets
-        FILE ${PACKAGE_NAME}-targets.cmake
-        DESTINATION ${CMAKE_INSTALL_LIBDIR}/cmake/${PACKAGE_NAME}
-        COMPONENT tests)
-
-    get_property(
-        _ROCP_SDK_TEST_TARGETS
-        DIRECTORY ${CMAKE_SOURCE_DIR}
-        PROPERTY rocprofiler-sdk-tests-targets)
-
-    if(_ROCP_SDK_TEST_TARGETS STREQUAL "_ROCP_SDK_TEST_TARGETS-NOTFOUND")
-        set(_ROCP_SDK_TEST_TARGETS "")
-    endif()
-
-    foreach(_TARG ${_ROCP_SDK_TEST_TARGETS})
-        list(APPEND PROJECT_INCLUDE_FILES "${_TARG}.cmake")
-    endforeach()
-endif()
+foreach(_TARG ${_ROCP_SDK_TEST_TARGETS})
+    list(APPEND PROJECT_INCLUDE_FILES "${_TARG}.cmake")
+endforeach()
 
 configure_package_config_file(
     ${PROJECT_SOURCE_DIR}/cmake/Templates/${PACKAGE_NAME}/config.cmake.in
     ${PROJECT_BINARY_DIR}/${CMAKE_INSTALL_LIBDIR}/cmake/${PACKAGE_NAME}/${PACKAGE_NAME}-config.cmake
     INSTALL_DESTINATION ${CMAKE_INSTALL_LIBDIR}/cmake/${PACKAGE_NAME}
     INSTALL_PREFIX ${CMAKE_INSTALL_PREFIX}
-    PATH_VARS PROJECT_INSTALL_DIR INCLUDE_INSTALL_DIR TEST_ROOT_DIR)
+    PATH_VARS PROJECT_INSTALL_DIR INCLUDE_INSTALL_DIR)
 
 write_basic_package_version_file(
     ${PROJECT_BINARY_DIR}/${CMAKE_INSTALL_LIBDIR}/cmake/${PACKAGE_NAME}/${PACKAGE_NAME}-config-version.cmake
@@ -72,12 +60,6 @@ install(
         ${PROJECT_BINARY_DIR}/${CMAKE_INSTALL_LIBDIR}/cmake/${PACKAGE_NAME}/${PACKAGE_NAME}-config.cmake
         ${PROJECT_BINARY_DIR}/${CMAKE_INSTALL_LIBDIR}/cmake/${PACKAGE_NAME}/${PACKAGE_NAME}-config-version.cmake
     DESTINATION ${CMAKE_INSTALL_LIBDIR}/cmake/${PACKAGE_NAME}
-    COMPONENT tests)
-
-rocprofiler_install_env_setup_files(
-    NAME ${PACKAGE_NAME}
-    VERSION ${PROJECT_VERSION}
-    INSTALL_DIR ${CMAKE_INSTALL_DATAROOTDIR}
     COMPONENT tests)
 
 # ------------------------------------------------------------------------------#
