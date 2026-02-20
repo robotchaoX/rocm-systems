ac4f0b4bf4e99d8477de907ac1aea7cbedb1854f
Shweta Khatri <shweta.khatri@amd.com>
2026-02-20 11:38:55 -0500

    rocrtst: CPUAccessToGPUMemoryTest: Cap host allocation to 512 MB under ASAN
    Under ASAN the allocator's shadow memory overhead can cause OOM
    on large-VRAM GPUs where pool_sz/2 exceeds host capacity.
    Detect AddressSanitizer at CMake configure time and define ROCRTST_ASAN.
    When ROCRTST_ASAN is defined, we cap host allocations to 512 MB.
    Non-ASAN builds are unchanged.

--- projects/rocr-runtime/rocrtst/suites/functional/memory_access.cc
+++ projects/rocr-runtime/rocrtst/suites/functional/memory_access.cc
@@ -58,6 +58,9 @@
 #include "gtest/gtest.h"
 #include "hsa/hsa.h"
 
+#ifdef ROCRTST_ASAN
+static const size_t kMaxTestAlloc = 512ULL * 1024 * 1024;
+#endif
 
 #define RET_IF_HSA_ERR(err) { \
   if ((err) != HSA_STATUS_SUCCESS) { \
@@ -394,6 +397,10 @@ void MemoryAccessTest::CPUAccessToGPUMemoryTest(hsa_agent_t cpuAgent,
       auto gran_sz = pool_i.alloc_granule;
       auto pool_sz = pool_i.size / gran_sz;
       auto max_alloc_size = pool_sz/2;
+#ifdef ROCRTST_ASAN
+      if (max_alloc_size > kMaxTestAlloc)
+        max_alloc_size = (kMaxTestAlloc / gran_sz) * gran_sz;
+#endif
       unsigned int max_element = max_alloc_size/sizeof(unsigned int);
       unsigned int *gpu_data;
       unsigned int *sys_data;
