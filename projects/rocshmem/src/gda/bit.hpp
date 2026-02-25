5789c108d8b40346a5a3aab1fac34517091dde22
Omri Mor <Omri.Mor@amd.com>
2026-02-25 01:27:05 +0000

    [rocshmem] gda: remove dependency on recent ROCm compiler builtin
    Signed-off-by: Omri Mor <Omri.Mor@amd.com>

--- projects/rocshmem/src/gda/bit.hpp
+++ projects/rocshmem/src/gda/bit.hpp
@@ -31,58 +31,116 @@
 
 namespace rocshmem {
 inline namespace _type_traits {
-  template <typename T>
-  struct is_unsigned_integer : std::bool_constant<std::is_integral_v<T> &&
-                                                  std::is_same_v<T, std::make_unsigned_t<T>>> { };
-  template <typename T>
-  inline constexpr bool is_unsigned_integer_v = is_unsigned_integer<T>::value;
+  // see [basic.fundamental]/p2 for definition of 'standard unsigned integer type'
+  inline namespace _standard_unsigned_integer {
+    template <typename T> struct is_standard_unsigned_integer           : std::false_type { };
+    template <> struct is_standard_unsigned_integer<unsigned char>      : std::true_type  { };
+    template <> struct is_standard_unsigned_integer<unsigned short>     : std::true_type  { };
+    template <> struct is_standard_unsigned_integer<unsigned int>       : std::true_type  { };
+    template <> struct is_standard_unsigned_integer<unsigned long>      : std::true_type  { };
+    template <> struct is_standard_unsigned_integer<unsigned long long> : std::true_type  { };
+
+    template <typename T>
+    inline constexpr bool is_standard_unsigned_integer_v = is_standard_unsigned_integer<T>::value;
+  }
 }
+
+namespace _template_builtin {
+  template <typename T> constexpr inline __host__ __device__
+  int _clzT(T val) noexcept {
+    if constexpr (std::is_same_v<T, unsigned long long>) {
+      return __builtin_clzll(val);
+    } else if constexpr (std::is_same_v<T, unsigned long>) {
+      return __builtin_clzl(val);
+    } else if constexpr (std::is_same_v<T, unsigned int>) {
+      return __builtin_clz(val);
+    } else if constexpr (std::is_same_v<T, unsigned short> ||
+                         std::is_same_v<T, unsigned char>) {
+      return __builtin_clz(static_cast<unsigned int>(val)) -
+             (std::numeric_limits<unsigned int>::digits - std::numeric_limits<T>::digits);
+    } else {
+      // sizeof(T) to force this to be instantiation-dependent
+      static_assert(sizeof(T) == 0, "clz not implemented for this type");
+    }
+  }
+
+  template <typename T> constexpr inline __host__ __device__
+  int _clzT(T val, int fallback) noexcept {
+    if (val == 0)
+      return fallback;
+    return _clzT(val);
+  }
+
+  template <typename T> constexpr inline __host__ __device__
+  int _ctzT(T val) noexcept {
+    if constexpr (std::is_same_v<T, unsigned long long>) {
+      return __builtin_ctzll(val);
+    } else if constexpr (std::is_same_v<T, unsigned long>) {
+      return __builtin_ctzl(val);
+    } else if constexpr (std::is_same_v<T, unsigned int>) {
+      return __builtin_ctz(val);
+    } else if constexpr (std::is_same_v<T, unsigned short> ||
+                         std::is_same_v<T, unsigned char>) {
+      return __builtin_ctz(static_cast<unsigned int>(val));
+    } else {
+      // sizeof(T) to force this to be instantiation-dependent
+      static_assert(sizeof(T) == 0, "ctz not implemented for this type");
+    }
+  }
+
+  template <typename T> constexpr inline __host__ __device__
+  int _ctzT(T val, int fallback) noexcept {
+    if (val == 0)
+      return fallback;
+    return _ctzT(val);
+  }
+}  // namespace _template_builtin
 
 template <typename T>
 constexpr inline __host__ __device__
-std::enable_if_t<is_unsigned_integer_v<T>, int>
+std::enable_if_t<is_standard_unsigned_integer_v<T>, int>
 countl_zero(T val) noexcept {
-  return __builtin_clzg(val, std::numeric_limits<T>::digits);
+  return _template_builtin::_clzT(val, std::numeric_limits<T>::digits);
 }
 
 template <typename T>
 constexpr inline __host__ __device__
-std::enable_if_t<is_unsigned_integer_v<T>, int>
+std::enable_if_t<is_standard_unsigned_integer_v<T>, int>
 countl_one(T val) noexcept {
   return countl_zero(static_cast<T>(~val));
 }
 
 template <typename T>
 constexpr inline __host__ __device__
-std::enable_if_t<is_unsigned_integer_v<T>, int>
+std::enable_if_t<is_standard_unsigned_integer_v<T>, int>
 countr_zero(T val) noexcept {
-  return __builtin_ctzg(val, std::numeric_limits<T>::digits);
+  return _template_builtin::_ctzT(val, std::numeric_limits<T>::digits);
 }
 
 template <typename T>
 constexpr inline __host__ __device__
-std::enable_if_t<is_unsigned_integer_v<T>, int>
+std::enable_if_t<is_standard_unsigned_integer_v<T>, int>
 countr_one(T val) noexcept {
   return countr_zero(static_cast<T>(~val));
 }
 
 template <typename T>
 constexpr inline __host__ __device__
-std::enable_if_t<is_unsigned_integer_v<T>, int>
+std::enable_if_t<is_standard_unsigned_integer_v<T>, int>
 bit_log2(T val) noexcept {
   return std::numeric_limits<T>::digits - 1 - countl_zero(val);
 }
 
 template <typename T>
 constexpr inline __host__ __device__
-std::enable_if_t<is_unsigned_integer_v<T>, int>
+std::enable_if_t<is_standard_unsigned_integer_v<T>, int>
 bit_width(T val) noexcept {
   return val == 0 ? 0 : bit_log2(val) + 1;
 }
 
 template <typename T>
 constexpr inline __host__ __device__
-std::enable_if_t<is_unsigned_integer_v<T>, T>
+std::enable_if_t<is_standard_unsigned_integer_v<T>, T>
 bit_ceil(T val) noexcept {
   if (val < 2)
     return 1;
@@ -91,7 +149,7 @@ bit_ceil(T val) noexcept {
 
 template <typename T>
 constexpr inline __host__ __device__
-std::enable_if_t<is_unsigned_integer_v<T>, T>
+std::enable_if_t<is_standard_unsigned_integer_v<T>, T>
 bit_floor(T val) noexcept {
   return val == 0 ? 0 : T{1} << bit_log2(val);
 }
